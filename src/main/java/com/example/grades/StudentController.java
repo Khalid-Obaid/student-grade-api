ebd06bddf50a361203a6bc29503de16862f96cd7
Khalid Obaid B Alharbi <khalidobaid42366@gmail.com>
2026-05-05 10:21:24 +0300

    Initial commit: Student Grade API

---
+++ src/main/java/com/example/grades/StudentController.java
@@ -0,0 +1,42 @@
+package com.example.grades;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(StudentController.class);
+
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student(1L, "Ahmed", "SE4111", 95.0),
+            new Student(2L, "Sara", "SE4111", 88.5)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getAll() {
+        return students;
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getById(@PathVariable Long id) {
+        return students.stream()
+                .filter(s -> s.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping("/students")
+    public Student add(@RequestBody Student s) {
+        s.setId((long) (students.size() + 1));
+        students.add(s);
+
+        logger.info("Added student: id={}, name={}", s.getId(), s.getName());
+
+        return s;
+    }
+}
